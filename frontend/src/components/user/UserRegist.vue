1cff422907822ae971bf6c64ea2402c2d47fb5f1
dkekzhs <68364199+dkekzhs@users.noreply.github.com>
2024-01-27 20:35:58 +0900

    [Test] ttt

--- frontend/src/components/user/UserRegist.vue
+++ frontend/src/components/user/UserRegist.vue
@@ -174,7 +174,7 @@ const duplicateIdCheck = async function(checkId) {
 
   /* code로 분기해야함 */
   await axios.post( // 로그인 콜백
-        `${import.meta.env.VITE_API_BASE_URL}`,
+        `${import.meta.env.VITE_API_BASE_URL}/user`,
         body,
         {
           headers: {
@@ -276,7 +276,7 @@ const duplicateNicknameCheck = async function(checkNickname) {
 
   /* code로 분기해야함 */
   await axios.post( // 로그인 콜백
-      `${ import.meta.env.VITE_API_BASE_URL }`,
+      `${ import.meta.env.VITE_API_BASE_URL }/user`,
         body,
         {
           headers: {
